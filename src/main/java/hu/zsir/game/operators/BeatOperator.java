062bbb222ed94643fe293457e629980fd17222ea
Sipos Ferenc <siposferenc93@gmail.com>
2015-05-05 19:56:40 +0200

    modify beatoperator and add test case

--- src/main/java/hu/zsir/game/operators/BeatOperator.java
+++ src/main/java/hu/zsir/game/operators/BeatOperator.java
@@ -5,38 +5,50 @@
  */
 package hu.zsir.game.operators;
 
+import hu.zsir.game.model.Card;
 import hu.zsir.game.model.Game;
 import hu.zsir.game.model.Number;
+import java.util.List;
 
 /**
  *
  * @author Feco
  */
 public class BeatOperator implements Operator {
-    
+
     private static BeatOperator beatoperator = new BeatOperator();
-    
+
     private BeatOperator() {
     }
-    
+
     @Override
     public boolean isApplicable(Game game) {
-        return game.getNextplayer().isChecked() || (game.getTable().getCards().size() > 0 && game.getTable().getCards().size() % 2 == 0 && (
-                game.getTable().getBottomCard().getNumber() != game.getTable().getTopCard().getNumber() && 
-                game.getTable().getTopCard().getNumber() != Number.HET ));
+        return game.getNextplayer().isChecked() || (game.getTable().getCards().size() > 0 && game.getTable().getCards().size() % 2 == 0 && (game.getTable().getBottomCard().getNumber() != game.getTable().getTopCard().getNumber()
+                && game.getTable().getTopCard().getNumber() != Number.HET));
     }
-    
+
     @Override
     public void apply(Game game) {
-        game.getTable().clear();
+        game.getCurrentplayer().addScore(computeScore(game.getTable().getCards()));
         game.getNextplayer().setCheck(false);
+        game.getTable().clear();
         if (DrawOperator.getDrawoperator().isApplicable(game)) {
             DrawOperator.getDrawoperator().apply(game);
         }
     }
-    
+
     public static BeatOperator getBeatoperator() {
         return beatoperator;
     }
-    
+
+    private int computeScore(List<Card> cards) {
+        int score = 0;
+        for (Card card : cards) {
+            if (card.getNumber() == Number.ASZ || card.getNumber() == Number.TIZ) {
+                score++;
+            }
+        }
+        return score;
+    }
+
 }
--- src/test/java/gameTest/BeatOperatorTest.java
+++ src/test/java/gameTest/BeatOperatorTest.java
@@ -5,8 +5,11 @@
  */
 package gameTest;
 
+import hu.zsir.game.model.Card;
 import hu.zsir.game.operators.BeatOperator;
 import hu.zsir.game.model.Game;
+import hu.zsir.game.model.Suit;
+import hu.zsir.game.model.Number;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,38 +20,59 @@ import static org.junit.Assert.*;
  * @author Feco
  */
 public class BeatOperatorTest {
-    
+
     private BeatOperator beatoperator;
     private Game game;
-    
+
     public BeatOperatorTest() {
     }
-    
+
+    private void initTable(Card... cards) {
+        for (Card card : cards) {
+            game.getTable().addCard(card);
+        }
+    }
+
     @Before
     public void setUp() {
         beatoperator = BeatOperator.getBeatoperator();
         game = new Game();
     }
-    
+
     @Test
     public void canBeat() {
         game.getNextplayer().check();
         assertTrue(beatoperator.isApplicable(game));
     }
-    
+
     @Test
     public void cantBeat() {
         assertFalse(beatoperator.isApplicable(game));
     }
-    
+
     @Test
     public void applyBeat() {
+        initTable();
         beatoperator.apply(game);
         assertTrue(game.getTable().getCards().isEmpty());
     }
-    
+
+    @Test
+    public void scoreTest() {
+        initTable(new Card(Number.ASZ, Suit.TOK));
+        beatoperator.apply(game);
+        assertEquals(1, game.getCurrentplayer().getScore());
+    }
+
+    @Test
+    public void scoreTest2() {
+        initTable(new Card(Number.ALSO, Suit.TOK));
+        beatoperator.apply(game);
+        assertEquals(0, game.getCurrentplayer().getScore());
+    }
+
     @After
     public void tearDown() {
     }
-    
+
 }
